8ff3b3a21463ac787129c8572b38189f13992151
aqntks <cjd0030@gmail.com>
2020-08-17 21:18:10 +0900

    basket.js 추가

---
+++ src/main/resources/static/app/basket.js
@@ -0,0 +1,18 @@
+function button_click(num) {
+    var data = {
+        consumerId: $('#consumerId' + num).val(),
+        menuId: $('#menuId' + num).val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/v1/basket',
+        dataType: 'json',
+        contentType:'application/json; charset=utf-8',
+        data: JSON.stringify(data)
+    }).done(function() {
+        alert('메뉴 추가');
+    }).fail(function (error) {
+        alert('오류 발생');
+    });
+}
