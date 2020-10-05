8563d7466b1b7f1510de0d9479c8d53ef8a72918
aqntks <cjd0030@gmail.com>
2020-10-06 02:15:26 +0900

    장바구니 DB저장 방법에서 SessionStorage 방법으로 변경

--- src/main/java/com/moskhu/web/MosController.java
+++ src/main/java/com/moskhu/web/MosController.java
@@ -1,6 +1,5 @@
 package com.moskhu.web;
 
-import com.moskhu.config.auth.dto.SessionUser;
 import com.moskhu.domain.cls.ConsumerOrder;
 import com.moskhu.domain.cls.MenuCount;
 import com.moskhu.domain.cls.MenuCountCheck;
@@ -16,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
 import java.util.*;
 
 @RequiredArgsConstructor
@@ -31,10 +29,6 @@ public class MosController {
     private final MenuRepository menuRepository;
     private final BasketRepository basketRepository;
 
-    private final HttpSession httpSession;
-
-
-
     @GetMapping("/") //시작 화면
     public String start(Model model) {
         if (statusService.existsById(1L)) {//status 데이터 존재 여부
@@ -45,47 +39,6 @@ public class MosController {
         } else
             return "preparing";
     }
-
-    /*@GetMapping("/home") //홈 화면
-    public String home(Model model) {
-        List<MenuListResponseDto> menu = menuService.findAllDesc();
-
-        List<MenuListResponseDto> type1 = new ArrayList<>();
-        List<MenuListResponseDto> type2 = new ArrayList<>();
-        List<MenuListResponseDto> type3 = new ArrayList<>();
-        List<MenuListResponseDto> type4 = new ArrayList<>();
-        List<MenuListResponseDto> type5 = new ArrayList<>();
-
-        for (MenuListResponseDto m : menu) {
-            switch (m.getMenuType()) {
-                case 1:
-                    type1.add(m);
-                    break;
-                case 2:
-                    type2.add(m);
-                    break;
-                case 3:
-                    type3.add(m);
-                    break;
-                case 4:
-                    type4.add(m);
-                    break;
-                case 5:
-                    type5.add(m);
-                    break;
-                default:
-            }
-        }
-
-
-      model.addAttribute("menu", menu);
-        model.addAttribute("type1", type1);
-        model.addAttribute("type2", type2);
-        model.addAttribute("type3", type3);
-        model.addAttribute("type4", type4);
-        model.addAttribute("type5", type5);
-        return "home";
-    }*/
 
     @GetMapping("/order") //주문 화면
     public String order(Model model) {
@@ -130,33 +83,6 @@ public class MosController {
 
     @GetMapping("/cart") //장바구니 화면
     public String cart(Model model) {
-        List<Basket> basket = basketRepository.findAllDesc();
-        List<MenuCount> list = new ArrayList<>();
-        //List 저장 인덱스 확인용
-        Map<String, Integer> check = new TreeMap<>();
-        int index = 0;
-        int totalPrice = 0;
-
-        for(Basket b : basket) {
-            Optional<Menu> menu = menuRepository.findById(b.getMenuId());
-            //이미 존재하는 경우
-            if(check.containsKey(menu.get().getMenuName())) {
-                list.set(check.get(menu.get().getMenuName()), new MenuCount(menu.get().getMenuName(), menu.get().getMenuPrice(), list.get(check.get(menu.get().getMenuName())).getCount()+1, b));
-
-            }
-            //아닌 경우
-            else{
-                check.put(menu.get().getMenuName(), index++);
-                list.add(new MenuCount(menu.get().getMenuName(), menu.get().getMenuPrice(), 1, b));
-            }
-        }
-
-        //총액
-        for(MenuCount mc : list)
-            totalPrice += mc.getAllCount();
-
-        model.addAttribute("basket", list);
-        model.addAttribute("totalPrice", totalPrice);
         return "cart";
     }
 
@@ -195,17 +121,6 @@ public class MosController {
     }
 
     ////////////////////////////////////////////////////////////////////////////// 판매자
-
-    @GetMapping("/seller_login") //판매자 로그인 화면
-    public String seller_login(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-        if (user != null){
-            model.addAttribute("userName", user.getName());
-        }
-
-        return "seller_login";
-    }
 
     @GetMapping("/seller_start") //판매자 시작 화면
     public String seller_start(Model model){
